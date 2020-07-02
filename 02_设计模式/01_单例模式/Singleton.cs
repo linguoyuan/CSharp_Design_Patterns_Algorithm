3b07b81e42bace9e332b40598db39aab3e4a8c89
林国源 <linguoyuan@dm-ai.cn>
2020-07-02 09:20:01 +0800

    add singleton

---
+++ 02_设计模式/01_单例模式/Singleton.cs
@@ -0,0 +1,23 @@
+﻿// 单例模式基类，继承它的子类为单例类
+using UnityEngine;
+namespace UDMAF
+{
+    public class Singleton<T>: MonoBehaviour where T: Singleton<T>
+    {
+        private static T _Instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    _Instance = FindObjectOfType<T>();
+                }
+                return _Instance;
+            }
+        }
+    }
+
+    
+}
---
+++ 02_设计模式/01_单例模式/SingletonClass1.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UDMAF
+{
+    public class SingletonClass1 : Singleton<SingletonClass1>
+    {
+        public void Test()
+        {
+            Debug.Log("111111");
+        }
+    }
+}
